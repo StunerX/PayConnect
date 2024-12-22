2e5fb05efdd26803feb01caf718be18224868667
Cleyton santos <cleyton.a.santos@gmail.com>
2024-12-22 13:16:01 -0300

    refactor: remove unused code

--- tests/PayConnect.Tests.Shared/CustomWebApplicationFactory.cs
+++ tests/PayConnect.Tests.Shared/CustomWebApplicationFactory.cs
@@ -9,14 +9,6 @@ public class CustomWebApplicationFactory<TStartup>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("E2ETest");
-        // builder.ConfigureServices(services =>
-        // {
-        //     var servicesProvider = services.BuildServiceProvider();
-        //     using var scope = servicesProvider.CreateScope();
-        //     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        //     dbContext.Database.EnsureDeleted();
-        //     dbContext.Database.EnsureCreated();
-        // });
 
         base.ConfigureWebHost(builder);
     }
